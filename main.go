5a8e68ce4e0495bfe5a0cd17759fb643c6a7e9dd
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2018-05-13 21:56:20 -0300

    chore: remove debug function

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/grvcoelho/169254/server"
 )
-
-func debug(x interface{}) {
-	fmt.Printf("%T: %#v\n", x, x)
-}
 
 func main() {
 	server := server.New()
